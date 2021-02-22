fe57d0c284fcb3b211521be66c296dc62ef8fd12
Mat Wilson-Platform <wilsom47@mc-n357035.home>
2021-02-22 16:39:24 +0000

    Added a simple multipler

---
+++ TestLang.js
@@ -0,0 +1,5 @@
+var x = 11
+var y = 4
+
+
+console.log(x % y);
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Welcome to the calculator!')
+const readline = require('readline-sync');
+
+console.log('Welcome to the calculator')
+
+console.log ('\nPlease enter the first number:');
+const firstArgument = readline.prompt()
+const firstNumber = firstArgument
+
+console.log ('\nPlease enter the second number:');
+const seondArgument = readline.prompt()
+const secondNumber = seondArgument
+
+const answer = (firstNumber * secondNumber)
+
+console.log ('\nThe answer is ' + answer)
